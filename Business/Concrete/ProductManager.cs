1094af28f20a5a842f0d171e0a96a23bb03e67fe
berkm <berkmahim620@gmail.com>
2024-12-11 16:16:16 +0300

    first commit

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,20 @@
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete;
+
+public class ProductManager: IProductService
+{
+    IProductDal _productDal;
+
+    public ProductManager(IProductDal productDal)
+    {
+        _productDal = productDal;
+    }
+
+    public List<Product> GetAll()
+    {
+        return _productDal.GetAll();
+    }
+}
